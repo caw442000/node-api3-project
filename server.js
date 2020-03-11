be97747c5f4f82d79eb88b8aeb08765693181cdf
cedric-winbush <cawinbushjr@gmail.com>
2020-03-11 13:12:09 -0500

    initial commit with logger

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+
+
 
 const server = express();
+
+//global middleware
+server.use(helmet());
+server.use(logger);
+
+server.use(express.json());
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +18,13 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+  const endpoint = req.originalUrl;
+
+  console.log(`${method} to ${endpoint}`)
+  next()
+
+}
 
 module.exports = server;
